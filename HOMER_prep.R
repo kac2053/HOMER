67cfaa70479255bb0f64a9824ebcd2e4fba53933
kac2053 <22157092+kac2053@users.noreply.github.com>
2019-11-12 18:25:16 -0500

    Update HOMER_prep.R

--- HOMER_prep.R
+++ HOMER_prep.R
@@ -6,32 +6,32 @@ library(GenomicRanges)
 library(dplyr)
 
 # Set working directory
-setwd("/Users/karen/mount/chuk/YTHDC1_HyperTRIBE/Project_10325_B/JAX_0381/")
+setwd("")
 
 # Import HyperTRIBE data
-df <- read.csv("/Users/karen/mount/chuk/YTHDC1_HyperTRIBE/Project_10325_B/JAX_0381/MOLM13_YTHDC1_snp_counts_snp_counts_significance_fpkm.csv")
+df <- read.csv("snp_counts_significance_fpkm.csv")
 
 # Prepare significant edit sites
 df.sig <- subset(df, p.adj < 0.05)
 df.sig$grange.start <- df.sig$start - 100
 df.sig$grange.end <- df.sig$start + 100
-hypertribe.cluster.df <- df.sig %>% select(c("seqnames", "grange.start", "grange.end", "strand", "gene.symbol"))
+cluster.df <- df.sig %>% select(c("seqnames", "grange.start", "grange.end", "strand", "gene.symbol"))
 
 # Prepare non-significant edit sites (background)
 df.not.sig <- subset(df, p.adj >= 0.05)
 df.not.sig$grange.start <- df.not.sig$start - 100
 df.not.sig$grange.end <- df.not.sig$start + 100
-hypertribe.cluster.df.not.sig <- df.not.sig %>% select(c("seqnames", "grange.start", "grange.end", "strand", "gene.symbol"))
+cluster.df.not.sig <- df.not.sig %>% select(c("seqnames", "grange.start", "grange.end", "strand", "gene.symbol"))
 
 # Convert dataframe to GenomicRanges object
-hypertribe.cluster.gr <- makeGRangesFromDataFrame(hypertribe.cluster.df,
+cluster.gr <- makeGRangesFromDataFrame(cluster.df,
                          keep.extra.columns=TRUE,
                          seqnames.field=c("seqnames"),
                          start.field="grange.start",
                          end.field=c("grange.end"),
                          strand.field="strand" )
 
-hypertribe.cluster.gr.not.sig <- makeGRangesFromDataFrame(hypertribe.cluster.df.not.sig,
+cluster.gr.not.sig <- makeGRangesFromDataFrame(cluster.df.not.sig,
                                                           keep.extra.columns=TRUE,
                                                           seqnames.field=c("seqnames"),
                                                           start.field="grange.start",
@@ -46,22 +46,22 @@ genome <- BSgenome.Hsapiens.UCSC.hg19 # mouse genome
 # Get sequence based on grange coordinates
 prepare.homer.input <- function(cluster.gr, cluster.df) {
   
-  hypertribe.cluster.sequence <- Biostrings::getSeq(genome, cluster.gr)
-  hypertribe.cluster.sequence.df <- as.data.frame(hypertribe.cluster.sequence)
-  rownames(hypertribe.cluster.sequence.df) <- paste(cluster.df$gene.symbol, 
+  cluster.sequence <- Biostrings::getSeq(genome, cluster.gr)
+  cluster.sequence.df <- as.data.frame(hypertribe.cluster.sequence)
+  rownames(cluster.sequence.df) <- paste(cluster.df$gene.symbol, 
                                                     cluster.df[,1],
                                                     cluster.df[,2],
                                                     cluster.df[,3],
-                                                    rownames(hypertribe.cluster.sequence.df),
+                                                    rownames(cluster.sequence.df),
                                                     sep=".")
-  colnames(hypertribe.cluster.sequence.df) <- "sequence"
+  colnames(cluster.sequence.df) <- "sequence"
   
-  return(hypertribe.cluster.sequence.df)
+  return(cluster.sequence.df)
   
 }
 
-hypertribe.cluster.sequence <- prepare.homer.input(hypertribe.cluster.gr, hypertribe.cluster.df)
-hypertribe.cluster.sequence.not.sig <- prepare.homer.input(hypertribe.cluster.gr.not.sig, hypertribe.cluster.df.not.sig)
+cluster.sequence <- prepare.homer.input(cluster.gr, cluster.df)
+cluster.sequence.not.sig <- prepare.homer.input(cluster.gr.not.sig, cluster.df.not.sig)
 
 
 # write fasta file using dataframe that has column names "ensembl_gene_id" and "3utr".
@@ -80,10 +80,10 @@ writeFasta<-function(data, filename){
 }
 
 # Write all sequences
-writeFasta(hypertribe.cluster.sequence,
-           paste0("MOLM13_YTHDC1_HyperTRIBE_significant_hypertribe_clusters_100bp_window.fa"))
-writeFasta(hypertribe.cluster.sequence.not.sig,
-           paste0("MOLM13_YTHDC1_HyperTRIBE_NOT_significant_hypertribe_clusters_100bp_window.fa"))
+writeFasta(cluster.sequence,
+           paste0("clusters_100bp_window.fa"))
+writeFasta(cluster.sequence.not.sig,
+           paste0("clusters_100bp_window.fa"))
 
 
 
